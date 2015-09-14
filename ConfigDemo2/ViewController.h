75c9c34a04eb34b3d018d113cb0e8c323ccb7042
Artem Vasnev <artem.vasnev@me.com>
2015-09-14 15:38:17 +0300

    Initial Commit

---
+++ ConfigDemo2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ConfigDemo2
+//
+//  Created by Artem on 9/14/15.
+//  Copyright (c) 2015 Artem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ConfigDemo2Tests/ConfigDemo2Tests.m
@@ -0,0 +1,40 @@
+//
+//  ConfigDemo2Tests.m
+//  ConfigDemo2Tests
+//
+//  Created by Artem on 9/14/15.
+//  Copyright (c) 2015 Artem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ConfigDemo2Tests : XCTestCase
+
+@end
+
+@implementation ConfigDemo2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
